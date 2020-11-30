eecd5332c9d90c7458900efbbf57dfdd0e7e5d0b
Armun Alam <armunalam@gmail.com>
2020-11-30 19:23:38 +0600

    moved chart function script.js

--- script.js
+++ script.js
@@ -14,3 +14,66 @@ function toggle_more() {
 //         alert("Hello World");
 //     }
 // });
+
+
+function lineChart(ctx, labelName, labelList, dataList, bdcolor, bgcolor) {
+    var chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: 'line',
+
+        // The data for our dataset
+        data: {
+            labels: labelList,
+            datasets: [{
+                label: labelName,
+                backgroundColor: bgcolor,
+                borderColor: bdcolor,
+                data: dataList
+            }]
+        },
+
+        // Configuration options go here
+        options: {}
+    });
+}
+
+// function lineChart1(ctx) {
+//     var chart = new Chart(ctx, {
+//         // The type of chart we want to create
+//         type: 'line',
+
+//         // The data for our dataset
+//         data: {
+//             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//             datasets: [{
+//                 label: 'Sample Dataset 1',
+//                 backgroundColor: '#d9e2ec',
+//                 borderColor: '#094480',
+//                 data: [2, 10, 5, 3, 20, 30, 45]
+//             }]
+//         },
+
+//         // Configuration options go here
+//         options: {}
+//     });
+// }
+// function lineChart2(ctx) {
+//     var chart = new Chart(ctx, {
+//         // The type of chart we want to create
+//         type: 'line',
+
+//         // The data for our dataset
+//         data: {
+//             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+//             datasets: [{
+//                 label: 'Sample Dataset 2',
+//                 backgroundColor: '#f0cdcd',
+//                 borderColor: '#962323',
+//                 data: [8, 5, 8, 9, 13, 11, 12]
+//             }]
+//         },
+
+//         // Configuration options go here
+//         options: {}
+//     });
+// }
